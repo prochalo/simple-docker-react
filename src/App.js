9e8c4917c49af1038b5f2dcb4012984dacd69ac8
chalothorn_c <chalothorn.cha@gmail.com>
2020-08-04 18:04:15 +0700

    initial  commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hi there Ohio New Poke
         </p>
         <a
           className="App-link"
